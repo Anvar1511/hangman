aa7df9b448b897471c17bfb5f95b8c3e8fcd3171
Anvar1511 <34777153+Anvar1511@users.noreply.github.com>
2021-05-12 05:53:56 +0500

    written stages of game

--- hangman.py
+++ hangman.py
@@ -12,6 +12,56 @@ chosen_word = random.choice(words_list)
 display = []
 word_len = len(chosen_word)
 
+
+stages = [''' +---+ 
+              |   |
+              O   | 
+             /|\  | 
+             /\   |
+                  |
+=========''', ''' 
+              +---+ 
+              |   |
+              O   | 
+             /|\  | 
+             /    |
+                  |
+
+=========''', ''' 
+              +---+ 
+              |   |
+              O   | 
+             /|\  | 
+                  |
+                  |
+
+=========''', ''' 
+              +---+ 
+              |   |
+              O   | 
+             /|   | 
+                  |
+                  |
+=========''', ''' 
+              +---+ 
+              |   |
+              O   | 
+             /    | 
+                  |
+                  |
+
+=========''', ''' 
+              +---+ 
+              |   |
+              O   | 
+                  | 
+                  |
+                  |
+=========''']
+
+
+lives = 6
+
 for _ in range(word_len):
     display+="_"
 print(display) 
@@ -25,7 +75,59 @@ while not end_of_game:
         letter = chosen_word[position]
         if letter == guess:
             display[position]=letter
+    if guess not in chosen_word:
+         
+         print(stages[lives-1])
+         lives -=1
+         if lives == 0:
+             end_of_game = True
+             print("You lose")
+             break
+    else:
+        pass
     print(display)
     if "_" not in display:
         end_of_game = True
         print("You win!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
